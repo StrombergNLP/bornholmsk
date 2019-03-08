c554905f8a48c8875ba2e31d530969d4c5bc7a07
Leon Derczynski <leonderczynski@gmail.com>
2019-03-08 17:16:08 +0300

    turn on word insertion; make sure right eval set it used

--- experiments.sh
+++ experiments.sh
@@ -3,13 +3,13 @@
 mkdir experiment/
 
 # generate bornholmsk embeddings
-echo "GENERATE CORPUS"
+echo "== GENERATE CORPUS"
 ./gen_bornholmsk_corpus.py > experiment/bo_corpus.txt
-echo "INDUCE SOURCE EMBEDDING"
+echo "== INDUCE SOURCE EMBEDDING"
 fasttext skipgram -input experiment/bo_corpus.txt -output experiment/bo.300d -minCount 1 -minn 2 -maxn 5 -dim 300
 
 # generate bilingual list
-echo "CREATE BILINGUAL MASTER LISTS AND PARTITIONS"
+echo "== CREATE BILINGUAL MASTER LISTS AND PARTITIONS"
 cat bo_da_word*tsv | sort -u | shuf > experiment/bo_da_word.tsv
 
 # split list into train & test
@@ -17,21 +17,21 @@ head -n 450 experiment/bo_da_word.tsv > experiment/bo_da_word_test.tsv
 tail -n +450 experiment/bo_da_word.tsv > experiment/bo_da_word_train.tsv
 
 # align embeddings: unsup, sup, combi
-echo "ALIGN: UNSUP"
-./align.py -s experiment/bo.300d.vec -t /datasets/cc.da.300.vec -o experiment/bornholmsk.300d.cc-da-aligned.unsup -u
+echo "== ALIGN: UNSUP"
+./align.py -s experiment/bo.300d.vec -t /datasets/cc.da.300.vec -o experiment/bornholmsk.300d.cc-da-aligned.unsup -u -i
 
-echo "ALIGN: SUP"
-./align.py -s experiment/bo.300d.vec -t /datasets/cc.da.300.vec -o experiment/bornholmsk.300d.cc-da-aligned.sup -d experiment/bo_da_word_train.tsv
+echo "== ALIGN: SUP"
+./align.py -s experiment/bo.300d.vec -t /datasets/cc.da.300.vec -o experiment/bornholmsk.300d.cc-da-aligned.sup -d experiment/bo_da_word_train.tsv -i
 
-echo "ALIGN: MIXED"
-./align.py -s experiment/bo.300d.vec -t /datasets/cc.da.300.vec -o experiment/bornholmsk.300d.cc-da-aligned.mixed -u -d experiment/bo_da_word_train.tsv
+echo "== ALIGN: MIXED"
+./align.py -s experiment/bo.300d.vec -t /datasets/cc.da.300.vec -o experiment/bornholmsk.300d.cc-da-aligned.mixed -u -d experiment/bo_da_word_train.tsv -i
 
 # evaluate embeddings w/ test set
-echo "EVAL: UNSUP"
+echo "== EVAL: UNSUP"
 ./eval_alignment.sh -s experiment/bornholmsk.300d.cc-da-aligned.unsup -t /datasets/cc.da.300.vec -d experiment/bo_da_word_test.tsv
 
-echo "EVAL: SUP"
-./eval_alignment.sh -s experiment/bornholmsk.300d.cc-da-aligned.unsup -t /datasets/cc.da.300.vec -d bo_da_word.tsv
+echo "== EVAL: SUP"
+./eval_alignment.sh -s experiment/bornholmsk.300d.cc-da-aligned.sup -t /datasets/cc.da.300.vec -d experiment/bo_da_word_test.tsv
 
-echo "EVAL: MIXED"
-./eval_alignment.sh -s experiment/bornholmsk.300d.cc-da-aligned.unsup -t /datasets/cc.da.300.vec -d bo_da_word.tsv
+echo "== EVAL: MIXED"
+./eval_alignment.sh -s experiment/bornholmsk.300d.cc-da-aligned.mixed -t /datasets/cc.da.300.vec -d experiment/bo_da_word_test.tsv
